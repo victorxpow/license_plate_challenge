4412c1aaeb3a2f7e6dbceeeeee20861e827ee18d
João <victorpow@live.com>
2021-02-22 13:47:37 -0300

    adding show view and test

--- app/controllers/license_plates_controller.rb
+++ app/controllers/license_plates_controller.rb
@@ -2,4 +2,8 @@ class LicensePlatesController < ApplicationController
   def index
     @license_plates = LicensePlate.all
   end
+
+  def show
+    @license_plate = LicensePlate.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'license_plates/index'
   root "license_plates#index"
 
-  resources :license_plates, only: [:index]
+  resources :license_plates, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
